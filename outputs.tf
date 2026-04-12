d1c9db4ea131fb1599bbbad623e2495d850c92f1
fabiomartinsbrrj <fabiomartinsbr@gmail.com>
2026-04-12 18:41:25 -0300

    feat: add nodes output with name-to-IP mapping

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,19 @@ output "private_ips" {
   value = [for i in aws_instance.nodes : i.private_ip]
 }
 
-output "ssh_commands" {
-  value = [
+output "nodes" {
+  value = {
     for i in aws_instance.nodes :
-    "ssh -i <path-da-sua-chave.pem> ubuntu@${i.public_ip}"
-  ]
+    i.tags["Name"] => {
+      public_ip  = i.public_ip
+      private_ip = i.private_ip
+    }
+  }
+}
+
+output "ssh_commands" {
+  value = {
+    for i in aws_instance.nodes :
+    i.tags["Name"] => "ssh -i <path-da-sua-chave.pem> ubuntu@${i.public_ip}"
+  }
 }
